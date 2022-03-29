81ea40356cd81395fefb13fb57c999139ea620a0
Monster Nary <hello@monsternary.com>
2022-03-29 10:52:17 +0700

    update new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./dist/**/*.{html,js}'],
   theme: {
-    colors: {
-      'primary': '#ED313F',
-      'secondary': '#EDECE5',
-      'text': '#040917',
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#ED313F',
+        'secondary': '#EDECE5',
+        'text': '#040917',
+      },
+    },
   },
   plugins: [],
 }
