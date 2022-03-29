95cfd61564758344f735f6530c718dbbfa47e2fc
Monster Nary <hello@monsternary.com>
2022-03-29 08:21:20 +0700

    added assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'primary': '#ED313F',
+      'secondary': '#EDECE5',
+      'text': '#040917',
+    },
     extend: {},
   },
   plugins: [],
